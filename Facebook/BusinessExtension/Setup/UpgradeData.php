00c7766f4f8a087f67597754c1c99364a64816a9
Xiangmin Xu <xiangminxu@fb.com>
2020-11-17 16:56:30 -0800

    change release tag to 1.2.0
    Summary: change release tag to 1.2.0
    
    Reviewed By: zlik
    
    Differential Revision: D25039892
    
    fbshipit-source-id: 2ede1e87

--- Facebook/BusinessExtension/Setup/UpgradeData.php
+++ Facebook/BusinessExtension/Setup/UpgradeData.php
@@ -118,84 +118,85 @@ class UpgradeData implements UpgradeDataInterface
         $categorySetup = $this->categorySetupFactory->create(['setup' => $setup]);
 
         $this->helper->log("getVersion". $context->getVersion());
-                $attributeConfig = $this->attributeConfig->getAttributesConfig();
-                foreach ($attributeConfig as $attrCode => $config) {
-                    // verify if already installed before
-                    if(!$eavSetup->getAttributeId(\Magento\Catalog\Model\Product::ENTITY, $attrCode)) {
-                        //Create the attribute
-                        $this->helper->log($attrCode. " not exist before, process it" );
-                        //  attribute does not exist
-                        // add a new attribute
-                        // and assign it to the "FacebookAttributeSet" attribute set
-                        $eavSetup->addAttribute(
-                            \Magento\Catalog\Model\Product::ENTITY,
-                            $attrCode,
-                            [
-                                'type' => $config['type'],
-                                'label' => $config['label'],
-                                'input' => $config['input'],
-                                'source' => $config['source'],
-                                'note' => $config['note'],
-                                'global' => ScopedAttributeInterface::SCOPE_GLOBAL,
-                                'required' => false,
-                                'user_defined' => true,
-                                'is_used_in_grid' => true,
-                                'is_visible_in_grid' => true,
-                                'is_filterable_in_grid' => true,
-                                'visible' => true,
-                                'is_html_allowed_on_front' => false,
-                                'searchable' => false,
-                                'filterable' => false,
-                                'comparable' => false,
-                                'visible_on_front' => false,
-                                'used_in_product_listing' => true,
-                                'unique' => false,
-                                'attribute_set' => 'FacebookAttributeSet' // assigning the attribute to the attribute set "FacebookAttributeSet"
-                            ]
-                        );
-                    }
-                    else{
-                        $this->helper->log($attrCode. " already installed, skip");
-                    }
+        if (version_compare($context->getVersion(), '1.2.0') < 0) {
+            $attributeConfig = $this->attributeConfig->getAttributesConfig();
+            foreach ($attributeConfig as $attrCode => $config) {
+                // verify if already installed before
+                if(!$eavSetup->getAttributeId(\Magento\Catalog\Model\Product::ENTITY, $attrCode)) {
+                    //Create the attribute
+                    $this->helper->log($attrCode. " not exist before, process it" );
+                    //  attribute does not exist
+                    // add a new attribute
+                    // and assign it to the "FacebookAttributeSet" attribute set
+                    $eavSetup->addAttribute(
+                        \Magento\Catalog\Model\Product::ENTITY,
+                        $attrCode,
+                        [
+                            'type' => $config['type'],
+                            'label' => $config['label'],
+                            'input' => $config['input'],
+                            'source' => $config['source'],
+                            'note' => $config['note'],
+                            'global' => ScopedAttributeInterface::SCOPE_GLOBAL,
+                            'required' => false,
+                            'user_defined' => true,
+                            'is_used_in_grid' => true,
+                            'is_visible_in_grid' => true,
+                            'is_filterable_in_grid' => true,
+                            'visible' => true,
+                            'is_html_allowed_on_front' => false,
+                            'searchable' => false,
+                            'filterable' => false,
+                            'comparable' => false,
+                            'visible_on_front' => false,
+                            'used_in_product_listing' => true,
+                            'unique' => false,
+                            'attribute_set' => 'FacebookAttributeSet' // assigning the attribute to the attribute set "FacebookAttributeSet"
+                        ]
+                    );
                 }
+                else{
+                    $this->helper->log($attrCode. " already installed, skip");
+                }
+            }
 
 
-                /**
-                 * Create a custom attribute group in all attribute sets
-                 * And, Add attribute to that attribute group for all attribute sets
-                 */
-                $attributeGroupName = $this->attributeConfig->getAttributeGroupName();
+            /**
+             * Create a custom attribute group in all attribute sets
+             * And, Add attribute to that attribute group for all attribute sets
+             */
+            $attributeGroupName = $this->attributeConfig->getAttributeGroupName();
 
-                // get the catalog_product entity type id/code
-                $entityTypeId = $categorySetup->getEntityTypeId(\Magento\Catalog\Model\Product::ENTITY);
+            // get the catalog_product entity type id/code
+            $entityTypeId = $categorySetup->getEntityTypeId(\Magento\Catalog\Model\Product::ENTITY);
 
-                // get the attribute set ids of all the attribute sets present in your Magento store
-                $attributeSetIds = $eavSetup->getAllAttributeSetIds($entityTypeId);
+            // get the attribute set ids of all the attribute sets present in your Magento store
+            $attributeSetIds = $eavSetup->getAllAttributeSetIds($entityTypeId);
 
-                foreach ($attributeSetIds as $attributeSetId) {
-                    $attr_group_sort_order = $this->getMinAttributeGroupSortOrder($eavSetup, $entityTypeId, $attributeSetId);
-                    $eavSetup->addAttributeGroup(
+            foreach ($attributeSetIds as $attributeSetId) {
+                $attr_group_sort_order = $this->getMinAttributeGroupSortOrder($eavSetup, $entityTypeId, $attributeSetId);
+                $eavSetup->addAttributeGroup(
+                    $entityTypeId,
+                    $attributeSetId,
+                    $attributeGroupName,
+                    $attr_group_sort_order // sort order
+                );
+
+                foreach ($attributeConfig as $attributeCode => $config) {
+                    // get the newly create attribute group id
+                    $attributeGroupId = $eavSetup->getAttributeGroupId($entityTypeId, $attributeSetId, $attributeGroupName);
+
+                    // add attribute to group
+                    $categorySetup->addAttributeToGroup(
                         $entityTypeId,
                         $attributeSetId,
-                        $attributeGroupName,
-                        $attr_group_sort_order // sort order
+                        $attributeGroupName, // attribute group
+                        $attributeCode,
+                        $config['sort_order']
                     );
-
-                    foreach ($attributeConfig as $attributeCode => $config) {
-                        // get the newly create attribute group id
-                        $attributeGroupId = $eavSetup->getAttributeGroupId($entityTypeId, $attributeSetId, $attributeGroupName);
-
-                        // add attribute to group
-                        $categorySetup->addAttributeToGroup(
-                            $entityTypeId,
-                            $attributeSetId,
-                            $attributeGroupName, // attribute group
-                            $attributeCode,
-                            $config['sort_order']
-                        );
-                    }
                 }
-//            }
+            }
+        }
 
         $setup->endSetup();
     }
