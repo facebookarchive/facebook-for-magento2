022920988dfa4eda8f4f068cf3838e9dc6ac07af
Xiangmin Xu <xiangminxu@fb.com>
2021-04-14 11:14:36 -0700

    use array instead of string for additional images in catalog push
    Summary:
    according to batch api dev doc, additional images is a array of string
    https://developers.facebook.com/docs/marketing-api/catalog-batch/reference#supported-fields-batch
    currently we are passing a string separated by `,` It seems working but to be safe, we should use array.
    
    Reviewed By: zlik
    
    Differential Revision: D27767114
    
    fbshipit-source-id: bfec15bb

--- Model/Product/Feed/Builder.php
+++ Model/Product/Feed/Builder.php
@@ -370,7 +370,7 @@ class Builder
             self::ATTR_SIZE                 => $this->getSize($product),
             self::ATTR_URL                  => $this->getProductUrl($product),
             self::ATTR_IMAGE_URL            => $imageUrl,
-            self::ATTR_ADDITIONAL_IMAGE_URL => implode(',', $images['additional_images']),
+            self::ATTR_ADDITIONAL_IMAGE_URL => $images['additional_images'],
         ];
 
         $this->enhancedCatalogHelper->assignECAttribute($product, $entry);
